21e6af268b4d2f784880ccb63e3867bfaa6f61cd
Abhi95d <abdabhishek195@gmail.com>
2025-04-28 11:39:41 +0530

    change the world

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -33,5 +33,5 @@ def vote(vote: schemas.Vote, db: session = Depends(database.get_db),
         vote_query.delete(synchronize_session = False)
         db.commit()
 
-        return {"message":"successfully deleted vote" }
+        return {"message":"successfully deleted vote......" }
 
